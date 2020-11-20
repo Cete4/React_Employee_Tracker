e40827315ba60b32547f20f1aa24a84bb36fe042
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-11-19 19:32:21 -0600

    Removed uneeded function

--- employee_tracker/src/Components/Directory/index.js
+++ employee_tracker/src/Components/Directory/index.js
@@ -23,18 +23,17 @@ class Directory extends Component {
             .catch((err) => console.error());
     }
 
-    handleInputChange = event => {
-        const value = event.target.value;
-        const name = event.target.name;
-        this.setState({
-            [name]: value
-        });
-    };
 
     // When the form is submitted, search the OMDB API for the value of `this.state.search`
     handleFormSubmit = event => {
         event.preventDefault();
         this.searchMovies(this.state.search);
+    };
+
+    searchMovies = query => {
+        API.getEmployee(query)
+            .then(res => this.setState({ result: res.data }))
+            .catch(err => console.log(err));
     };
 
     render() {
@@ -44,7 +43,10 @@ class Directory extends Component {
                     <h1 id="header">Employee Tracker</h1>
                 </div>
                 <div className="input-group mx-auto">
-                    <input onChange={this.handleInputChange} name="search" value={this.state.search} type="text" className="form-control" placeholder="Search" />
+                    <input name="search" value={this.state.search} type="text" className="form-control" placeholder="Search" />
+                    <div className="input-group-append">
+                        <button onClick={this.handleFormSubmit} className="btn btn-primary mt-3">Search </button>
+                    </div>
                 </div>
                 <div>
                     <Table striped bordered hover>
